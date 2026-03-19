e3017ed8602989383399a89e3d80d230423d9864
Santiago <01hanssantiago@gmail.com>
2026-03-19 14:11:13 -0400

    RS-336: Crear DisponibilidadController con validaciones y repositorio

---
+++ GestionSistemas/src/main/java/com/residencial/GestionSistemas/GestionSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.residencial.GestionSistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSistemasApplication.class, args);
+	}
+
+}
