caf923ee2c114c74400c34d2ddc743499f9ba402
Santiago <01hanssantiago@gmail.com>
2026-03-19 23:32:47 -0400

    RS-336 Inicializacion de Spring Boot

---
+++ GestionSistemas/src/test/java/com/residencial/GestionSistemas/GestionSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residencial.GestionSistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
