2f480f555a55ce8233aaea626148736385e8a514
Santiago <01hanssantiago@gmail.com>
2026-03-19 23:33:26 -0400

    RS-336 Agregar Controlador, Servicio y Repositorio

---
+++ GestionSistemas/src/main/java/com/residencial/GestionSistemas/repository/HabitacionRepository.java
@@ -0,0 +1,17 @@
+package com.residencial.GestionSistemas.repository;
+
+import com.residencial.GestionSistemas.model.Habitacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HabitacionRepository extends JpaRepository<Habitacion, Long> {
+
+    // Consulta SQL automática de Spring: Filtra por Tipo y Estado
+    @Query("SELECT h FROM Habitacion h WHERE h.tipo = :tipo AND h.estado = 'Disponible'")
+    List<Habitacion> findDisponiblesPorTipo(@Param("tipo") String tipo);
+}
